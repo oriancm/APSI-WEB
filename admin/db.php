8f70ed2444bb8728348e6b2fb13cb13a5353b5af
orian <30657132+oriancm@users.noreply.github.com>
2025-05-03 01:14:31 -0700

    sql

--- admin/db.php
+++ admin/db.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:host=127.0.0.1;dbname=apsi;charset=utf8mb4','root','');
+    $db = new PDO('mysql:host=127.0.0.1;dbname=apsi;charset=utf8mb4','root','root');
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
