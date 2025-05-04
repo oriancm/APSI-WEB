756865be785df4a59fed774ae9946684901c6d95
orian <30657132+oriancm@users.noreply.github.com>
2025-05-03 17:17:02 -0700

    docker

--- admin/db.php
+++ admin/db.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:host=i4kc8k04k00wc00sswk8oswg;port=3306;dbname=default;charset=utf8mb4', 'root', 'root');
+    $db = new PDO('mysql:host=apsi;port=3306;dbname=default;charset=utf8mb4', 'root', 'root');
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
