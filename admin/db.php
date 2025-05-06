d57b5d0c126ea97f26ac3be90ae4c919ddd06613
orian <30657132+oriancm@users.noreply.github.com>
2025-05-06 16:55:22 -0700

    db

--- admin/db.php
+++ admin/db.php
@@ -4,11 +4,9 @@ try {
         'mysql:host=apsi;port=3306;dbname=default;charset=utf8mb4',
         'root',
         'root',
-        // [
-        //     PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/coolify-ca.crt',
-        //     PDO::MYSQL_ATTR_SSL_CERT => '/etc/ssl/certs/server.crt',
-        //     PDO::MYSQL_ATTR_SSL_KEY => '/etc/ssl/certs/server.key',
-        // ]
+        [
+            PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/coolify-ca.crt',
+        ]
     );
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully!";
